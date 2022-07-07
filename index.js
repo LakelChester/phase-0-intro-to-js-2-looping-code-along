1c6d1340f470f60412bf86cf7f4dd64e558013a5
LakelChester <lakel@chesterassociate.com>
2022-07-06 18:53:01 -0700

    commit tree a390f9d7e8ff7c7164251ed3443f8664651b321b

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solutions in this file
+const emptyArray =[]
+const names =["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(names, event){
+
+    for (let i = 0; i < names.length; i++) {
+
+       emptyArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+      
+    }
+
+    return emptyArray;      
+}
+
+
+
+
+function countDown(number){
+    let downCount = 10; 
+    while (downCount >= 0){
+        console.log(downCount--)
+    }
+
+}
+
+
+
+
+
+
+
+
+
